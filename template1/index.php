256c7d601fb5147ce6dce83e603fc6ca645b019c
Chris Bier <cbier@mednet-tech.com>
2013-02-20 17:54:09 -0500

    Initial Commit

---
+++ template1/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+
+      body{
+        background-color:#333333;
+        margin: 0;
+      }
+
+      .header-content{
+        margin: -179px 0 0 0;
+        width:775px;
+      }
+
+      .address{
+        text-align: right;
+        margin: 0 50px 0 0;
+      }
+
+      .logo{
+        margin: 0 0 0 50px;
+      }
+
+      .h1{
+        font-size: 2em;
+        color: #000000;
+        margin: 20px 0 0;
+      }
+
+      .h2{
+        font-size: 1.5em;
+        color: #555555;
+        margin: 24px 0 0;
+      }
+
+      .pic{
+        margin: 6px 0 12px;
+      }
+
+      .content{
+        background-color: #ffffff;
+        width: 462px; 
+        padding: 10px 30px 25px;
+      }
+
+      .sidebar{
+        width: 275px;
+        background-color: #d9d9d9;
+        width: 193px;
+        padding: 10px 30px 25px;
+      }
+
+      .hr{
+         height: 1px;
+         background-color: #aaaaaa;
+      }
+
+      .patient-portal{
+        margin: 44px 0 24px;
+      }
+
+      .patient-portal a{
+        padding:10px 15px;
+        color:#ffffff;
+        font-size:1.5em;
+        text-decoration:none;
+        margin:20px 0 0 20px;
+      }
+
+      .copyright{ 
+        color:#ffffff;
+        font-size:12px;
+        margin:-47px 0 0 40px;
+      }
+
+    </style>
+
+  </head>
+
+  <body>
+
+      <table width="775" border="0" align="center" cellpadding="0" cellspacing="0">
+
+        <tr>
+          <td colspan="2" height="179" class="header">
+            <img src="images/header.png" width="775" height="179" border="0" />
+            
+            <table border="0" cellpadding="0" cellspacing="0" height="179" class="header-content">
+              <tr>
+                <td>   
+                  <img src="LOGO_HERE" class="logo" />
+                </td>
+                <td>
+                  <p class="address">
+                    123 Street Name
+                    <br />Hicksville, NY 11801
+                  </p>
+                </td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+
+        <tr>
+
+          <td class="content" valign="top">
+            <p class="h1">Lorem Ipsum</p>
+            <p><img src="../../../public/images/eNewsletter-thumbnail.jpg" align="right" class="pic"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vehicula tellus sit amet enim ornare vehicula. Nullam magna lectus, condimentum non blandit a, blandit in lorem. Cras venenatis nisl sed mauris varius aliquam. In hac habitasse platea dictumst. Integer felis lacus, viverra ac blandit quis, vehicula vel metus. Pellentesque egestas rhoncus nibh. Proin eu nunc est. Phasellus elementum viverra lectus id posuere. Duis facilisis libero tellus, vel gravida nunc. Donec lacinia elementum ante, sit amet mollis arcu rhoncus sit amet. Integer adipiscing eros a sem vestibulum ultricies. In sagittis fringilla dui, vitae vulputate orci commodo eget. Nulla pretium ullamcorper neque, semper porttitor diam accumsan nec. Donec in metus libero, lacinia tincidunt elit. Ut vitae aliquam leo. Sed molestie nunc ut nisi facilisis ac facilisis ante auctor.</p>
+
+            <p>Morbi libero neque, semper vitae tempor at, mattis sit amet lorem. Integer nec eros nec massa dignissim pretium. Nam dignissim euismod tortor, fringilla posuere neque tristique in. Fusce sed odio quam, non sollicitudin sapien. Suspendisse tristique, neque id feugiat suscipit, enim nulla laoreet tortor, vitae pulvinar diam ligula eget nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum faucibus vestibulum sem eu gravida.</p>
+          </td>
+          
+          <td class="sidebar" valign="top">
+            <p class="h2">About Us</p>
+            <p>Morbi libero neque, semper vitae tempor at, mattis sit amet lorem. Integer nec eros nec massa dignissim pretium. Nam dignissim euismod tortor, fringilla posuere neque.</p>
+            <p class="hr"></p>
+
+            <p class="h2">Address &amp; Map</p>
+            <p>Morbi libero neque, semper vitae tempor at, mattis sit amet lorem. Integer nec eros nec massa dignissim pretium. Nam dignissim euismod tortor, fringilla posuere neque.</p>
+            <p class="hr"></p>
+
+            <p class="h2">Spotlight</p>
+            <p>Morbi libero neque, semper vitae tempor at, mattis sit amet lorem. Integer nec eros nec massa dignissim pretium. Nam dignissim euismod tortor, fringilla posuere neque.</p>
+            <p class="hr"></p>
+
+            <p class="patient-portal"><a href="#">Patient Portal</a></p>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="2">
+            <img src="images/footer.jpg" width="775" height="82" border="0" />
+            <p class="copyright">Copyright (c) 2013 - Company Name, Inc.</p>
+          </td>
+        </tr>
+
+      </table>
+
+  </body>
+</html>
